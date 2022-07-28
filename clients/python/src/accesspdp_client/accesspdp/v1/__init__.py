52fbff3af70cd747e2f6b9adbf0162c794513571
Ben Leggett <bleggett@virtru.com>
2022-07-28 13:36:43 -0400

    Hum

--- clients/python/src/accesspdp_client/accesspdp/v1/__init__.py
+++
@@ -1,6 +0,0 @@
-# Hack because
-# 1. Python still has a poopy module import system that doesn't rely on canonical identifiers
-# and never quite works correctly in all cases
-# 2. gRPC's python codegen sucks
-import sys,os
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.pardir,os.pardir))
